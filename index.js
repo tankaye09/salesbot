47f0e60765c1ab135f6f69c4100fb19e6021496c
Kaye <tankaye@gmail.com>
2022-04-03 20:56:54 +0800

    debug

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ function handleMessage(sender_psid, received_message) {
   // Check if the message contains text
   if (received_message.text) {
     // Create the payload for a basic text message
-    "response" = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`,
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
     };
   }
 
