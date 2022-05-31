5826ac3cea4b7ee7d6de52403775aca080d3fe3d
tankaye09 <tankaye@gmail.com>
2022-05-31 17:11:20 +0800

    merge

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+module.exports = {
+  query: (text, values) => {
+    const client = await pool.connect();
+    client
+        .query(text, values)
+        .then(res => {
+        console.log(res.rows[0])
+        // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+        return res
+        })
+        .catch(e => console.error(e.stack)) 
+  },
+};
