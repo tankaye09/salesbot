6c683eb8d486479d1fb40104ead6a55dfd9beeb5
tankaye09 <tankaye@gmail.com>
2022-08-04 20:15:03 +0800

    clean up

--- index.js
+++ index.js
@@ -270,7 +270,6 @@ async function sendToRasa(senderPsid, webhookEvent) {
             };
             // Check if button is url_button
             if (messengerButtonObj["payload"].slice(0, 4) == "http") {
-              console.log("I am a URL BUTTON");
               messengerButtonObj["type"] = "web_url";
               messengerButtonObj["url"] = buttonObj["payload"];
               delete messengerButtonObj.payload;
